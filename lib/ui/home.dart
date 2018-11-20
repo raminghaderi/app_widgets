240c3a93fdf7c480aef1f8db40ed63ebd65fc752
Ramin Ghaderi <raminghaderi77@gmail.com>
2018-11-20 13:40:40 +0100

    Adding Style to text

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -3,13 +3,20 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Container(
-      alignment: Alignment.center,
-      color: Colors.deepOrangeAccent,
-      /*width: 192.0,
-      height: 96.0,
-      margin: EdgeInsets.all(50.0),*/
-      child: Text('Pizza'),
+    return Center(
+      child: Container(
+        alignment: Alignment.center,
+        color: Colors.deepOrangeAccent,
+        /*width: 192.0,
+        height: 96.0,
+        margin: EdgeInsets.all(50.0),*/
+        child: Text('Pizza',
+          style: TextStyle(
+            fontSize: 80,
+            decoration: TextDecoration.none,
+            fontFamily: 'Oxygen',
+            fontWeight: FontWeight.normal
+          ),),
     )
     );
   }
