a2a85ebc2416732897977ade72202d16dd18ff57
Ramin Ghaderi <raminghaderi77@gmail.com>
2018-11-20 13:18:56 +0100

    Initial commit.

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import './ui/home.dart';
+
+void main() {
+  runApp(
+    new MaterialApp(
+      title: 'App Widgets',
+      home: new Home(),
+    )
+  );
+}
